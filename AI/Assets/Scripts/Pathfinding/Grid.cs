31b06d4f086c33ccbe9a73761d8ebe426e67175f
Kyle Sheridan <55580454+KyleSheridan@users.noreply.github.com>
2021-12-08 18:59:59 +0000

    Pathfinding works with agents

--- AI/Assets/Scripts/Pathfinding/Grid.cs
+++ AI/Assets/Scripts/Pathfinding/Grid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public bool onlyDisplayPathGizmos;
+    public bool displayGridGizmos;
     public Transform player;
     public LayerMask unwalkableMask;
     public Vector2 gridSize;
@@ -15,7 +15,7 @@ public class Grid : MonoBehaviour
     int gridSizeX, gridSizeY;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridSize.x / nodeDiameter);
@@ -91,36 +91,17 @@ public class Grid : MonoBehaviour
         return grid[x, y];
     }
 
-    public List<Node> path;
-
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, 1, gridSize.y));
-        if (onlyDisplayPathGizmos)
+        if (grid != null && displayGridGizmos)
         {
-            if (path != null)
-            {
-                foreach (Node n in path)
-                {
-                    Gizmos.color = Color.magenta;
-                    Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
-                }
-            }
-        }
-        else
-        {
-            if (grid != null)
-            {
-                //Node playerNode = NodeFromWorldPoint(player.position);
+            //Node playerNode = NodeFromWorldPoint(player.position);
 
-                foreach (Node n in grid)
-                {
-                    Gizmos.color = (n.walkable) ? Color.white : Color.red;
-                    if (path != null)
-                        if (path.Contains(n))
-                            Gizmos.color = Color.magenta;
-                    Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
-                }
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
             }
         }
     }
--- AI/Assets/Scripts/Pathfinding/Pathfinding.cs
+++ AI/Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -2,95 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Diagnostics;
+using System;
 
 public class Pathfinding : MonoBehaviour
 {
-    public Transform seeker, target;
+    PathRequestManager requestManager;
 
     Grid grid;
 
     private void Awake()
     {
+        requestManager = GetComponent<PathRequestManager>();
         grid = GetComponent<Grid>();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartFindPath(Vector3  startPos, Vector3 targetPos)
     {
-
+        StartCoroutine(FindPath(startPos, targetPos));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            FindPath(seeker.position, target.position);        
-        }
-    }
-
-    void FindPath(Vector3 startPos, Vector3 endPos)
+    IEnumerator FindPath(Vector3 startPos, Vector3 endPos)
     {
         Stopwatch sw = new Stopwatch();
         sw.Start();
 
+        Vector3[] waypoints = new Vector3[0];
+        bool pathSuccess = false;
+
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(endPos);
 
-        //List<Node> openSet = new List<Node>();
-
-        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
-        HashSet<Node> closedSet = new HashSet<Node>();
-
-        openSet.Add(startNode);
-
-        while (openSet.Count > 0)
+        if(startNode.walkable && targetNode.walkable)
         {
-            //Node currentNode = openSet[0];
-            //for (int i = 1; i < openSet.Count; i++)
-            //{
-            //    if(openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
-            //    {
-            //        currentNode = openSet[i];
-            //    }
-            //}
+            Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+            HashSet<Node> closedSet = new HashSet<Node>();
 
-            //openSet.Remove(currentNode);
-            Node currentNode = openSet.RemoveFirst();
+            openSet.Add(startNode);
 
-            closedSet.Add(currentNode);
-
-            if(currentNode == targetNode)
+            while (openSet.Count > 0)
             {
-                sw.Stop();
-                print("Path found: " + sw.ElapsedMilliseconds + "ms");
-                RetracePath(startNode, targetNode);
-                return;
-            }
+                Node currentNode = openSet.RemoveFirst();
 
-            foreach(Node neighbour in grid.GetNeighbours(currentNode))
-            {
-                if (!neighbour.walkable || closedSet.Contains(neighbour))
-                    continue;
+                closedSet.Add(currentNode);
 
-                int newMovementCost = currentNode.gCost + GetDistance(currentNode, neighbour);
-                if(newMovementCost < neighbour.gCost || !openSet.Contains(neighbour))
+                if(currentNode == targetNode)
                 {
-                    neighbour.gCost = newMovementCost;
-                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    sw.Stop();
+                    print("Path found: " + sw.ElapsedMilliseconds + "ms");
+                    pathSuccess = true;
+                    break;
+                }
 
-                    neighbour.parent = currentNode;
+                foreach(Node neighbour in grid.GetNeighbours(currentNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                        continue;
 
-                    if (!openSet.Contains(neighbour))
+                    int newMovementCost = currentNode.gCost + GetDistance(currentNode, neighbour);
+                    if(newMovementCost < neighbour.gCost || !openSet.Contains(neighbour))
                     {
-                        openSet.Add(neighbour);
+                        neighbour.gCost = newMovementCost;
+                        neighbour.hCost = GetDistance(neighbour, targetNode);
+
+                        neighbour.parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
                     }
                 }
             }
         }
+        yield return null;
+
+        if (pathSuccess)
+        {
+            waypoints = RetracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
     }
 
-    void RetracePath(Node startNode, Node endNode)
+    Vector3[] RetracePath(Node startNode, Node endNode)
     {
         List<Node> path = new List<Node>();
         Node currentNode = endNode;
@@ -100,9 +93,27 @@ public class Pathfinding : MonoBehaviour
             path.Add(currentNode);
             currentNode = currentNode.parent;
         }
-        path.Reverse();
+        Vector3[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
 
-        grid.path = path;
+        return waypoints;
+    }
+
+    Vector3[] SimplifyPath(List<Node> path)
+    {
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+
+        for (int i = 1; i < path.Count; i++)
+        {
+            Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+            if(directionNew != directionOld)
+            {
+                waypoints.Add(path[i - 1].worldPos);
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
     }
 
     int GetDistance(Node nodeA, Node nodeB)
